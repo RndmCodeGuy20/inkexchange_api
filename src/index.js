5c8956fe397d3db0b8a71bb3cd477b74b0d32f6c
maneshantanu20 <manesg@rknec.edu>
2023-04-22 09:56:21 +0530

    docs(index) 🎓 : documentation added to init functions : master

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import { envConfig } from './config/index';
 import { getConnection } from '#helpers/database';
 import { logger } from '#helpers/logger';
 
+/**
+ * @description - init function is the entry point of the application,
+ * it connects to the database and starts the server.
+ * @return {Promise<void>}
+ */
 const init = async () => {
   await getConnection();
   app.listen(envConfig.APP_PORT, () => {
@@ -10,6 +15,13 @@ const init = async () => {
   });
 };
 
+/**
+ * @description - start the server, handles promise rejections
+ * and exits the process if an error occurs.
+ */
 init().then(() => {
   logger.info('Server started successfully');
+}).catch((error) => {
+  logger.error(error);
+  process.exit(1);
 });
