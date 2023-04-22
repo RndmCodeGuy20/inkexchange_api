0f5df9cfed28553d31c98788da5f9b3e556bbfaf
maneshantanu20 <manesg@rknec.edu>
2023-04-22 09:51:22 +0530

    refactor(index) 🎓 : removed unused variables from index : master

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import { logger } from '#helpers/logger';
 const init = async () => {
   await getConnection();
   app.listen(envConfig.APP_PORT, () => {
-    console.log(`Listening on ${envConfig.HOSTNAME} http://localhost:${envConfig.APP_PORT}`);
+    logger.info(`Listening on ${envConfig.HOSTNAME} http://localhost:${envConfig.APP_PORT}`);
   });
 };
 
 init().then(() => {
-  logger.debug('Server started successfully');
+  logger.info('Server started successfully');
 });
