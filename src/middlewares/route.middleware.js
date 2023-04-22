c9750a40a7720c21a1befabee94311dd9abf1b25
maneshantanu20 <manesg@rknec.edu>
2023-04-22 09:30:56 +0530

    style(route) 📰 : code style changed, linting incorporated : master

--- src/middlewares/route.middleware.js
+++ src/middlewares/route.middleware.js
@@ -5,6 +5,11 @@ import { ERROR_CODES } from '#constants/index';
  * Error Middleware for NotAllowed
  */
 class MethodNotAllowedError extends Error {
+  /**
+	 * @param {string} message
+	 * @param {number} httpStatus
+	 * @param {string} errorCode
+	 */
   constructor(message, httpStatus, errorCode) {
     super(message);
     this.name = 'MethodNotAllowedError';
@@ -15,13 +20,29 @@ class MethodNotAllowedError extends Error {
 
 export const methodNotAllowed = () => {
   try {
-    throw new MethodNotAllowedError(ReasonPhrases.METHOD_NOT_ALLOWED, StatusCodes.METHOD_NOT_ALLOWED, ERROR_CODES.NOT_ALLOWED);
+    throw new MethodNotAllowedError(
+        ReasonPhrases.METHOD_NOT_ALLOWED,
+        StatusCodes.METHOD_NOT_ALLOWED,
+        ERROR_CODES.NOT_ALLOWED,
+    );
   } catch (error) {
     throw error;
   }
 };
 
+/**
+ * Error Middleware for RouteNotFound
+ * @param {string} message
+ * @param {number} httpStatus
+ * @param {string} errorCode
+ * @return {Error}
+ */
 class RouteNotFoundError extends Error {
+  /**
+	 * @param {string} message
+	 * @param {number} httpStatus
+	 * @param {string} errorCode
+	 */
   constructor(message, httpStatus, errorCode) {
     super(message);
     this.name = 'RouteNotFoundError';
@@ -32,7 +53,11 @@ class RouteNotFoundError extends Error {
 
 export const routeNotFound = () => {
   try {
-    throw new RouteNotFoundError(ReasonPhrases.NOT_FOUND, StatusCodes.NOT_FOUND, ERROR_CODES.NOT_FOUND);
+    throw new RouteNotFoundError(
+        ReasonPhrases.NOT_FOUND,
+        StatusCodes.NOT_FOUND,
+        ERROR_CODES.NOT_FOUND,
+    );
   } catch (error) {
     throw error;
   }
