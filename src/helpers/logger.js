b04447792c4d24e4a209f45f6f158927d9caffbc
maneshantanu20 <manesg@rknec.edu>
2023-05-22 10:10:33 +0530

    refactor(logger) 📰 : removed unnecessary comments : master

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -5,53 +5,52 @@ const pretty = require('pino-pretty');
 const pino = require('pino');
 const pinoExpress = require('express-pino-logger');
 
-const level = envConfig.ENV === ENVIRONMENTS.PRODUCTION ? LOG_LEVELS.INFO : LOG_LEVELS.DEBUG;
-// const prettyPrint = {
-//   colorize: 'true',
-//   levelFirst: true,
-//   translateTime: 'SYS.standard',
-//   ignore: 'pid, hostname, module',
-// };
+const level =
+	envConfig.ENV === ENVIRONMENTS.PRODUCTION ?
+		LOG_LEVELS.INFO :
+		LOG_LEVELS.DEBUG;
 
-export const logger = pino({
-  name: 'server',
-  level: level || LOG_LEVELS.DEBUG,
-  formatters: {
-    level(label) {
-      return { level: label };
+export const logger = pino(
+    {
+      name: 'server',
+      level: level || LOG_LEVELS.DEBUG,
+      formatters: {
+        level(label) {
+          return { level: label };
+        },
+      },
     },
-  },
-  // prettyPrint: envConfig.ENV === ENVIRONMENTS.PRODUCTION ? false : prettyPrint,
+    pretty({
+      colorize: true,
+      singleLine: false,
+      translateTime: 'SYS.standard',
+      ignore: 'pid, hostname, module',
+    }),
+);
 
-}, pretty({
-  colorize: true,
-  singleLine: false,
-  translateTime: 'SYS.standard',
-  ignore: 'pid, hostname, module',
-}));
-
-
-export const expressLogger = pinoExpress({
-  name: 'express',
-  level: level || LOG_LEVELS.INFO,
-  formatters: {
-    level(label) {
-      return { level: label };
+export const expressLogger = pinoExpress(
+    {
+      name: 'express',
+      level: level || LOG_LEVELS.INFO,
+      formatters: {
+        level(label) {
+          return { level: label };
+        },
+      },
+      serializers: {
+        res: (res) => ({
+          status: res.statusCode,
+        }),
+        req: (req) => ({
+          method: req.method,
+          url: req.url,
+        }),
+      },
     },
-  },
-  serializers: {
-    res: (res) => ({
-      status: res.statusCode,
+    pretty({
+      colorize: true,
+      singleLine: false,
+      translateTime: 'SYS.standard',
+      ignore: 'pid, hostname, module',
     }),
-    req: (req) => ({
-      method: req.method,
-      url: req.url,
-    }),
-  },
-  // prettyPrint: envConfig.ENV === ENVIRONMENTS.PRODUCTION ? false : prettyPrint,
-}, pretty({
-  colorize: true,
-  singleLine: false,
-  translateTime: 'SYS.standard',
-  ignore: 'pid, hostname, module',
-}));
+);
