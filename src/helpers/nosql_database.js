f48d0a4032de4b86a77361bb75a37ef805cfbee1
maneshantanu20 <manesg@rknec.edu>
2023-07-19 22:30:31 +0530

    chore(env) 👨‍💼 : db name added to config and env

--- src/helpers/nosql_database.js
+++ src/helpers/nosql_database.js
@@ -5,14 +5,13 @@ import { envConfig } from '#configs/index';
 export const getMongoConnection = async () => {
   try {
     // eslint-disable-next-line max-len
-    const mongoUrl = `mongodb+srv://${envConfig.MONGO_DB_USERNAME}:${envConfig.MONGO_DB_PASSWORD}@clusterinkexchange.9jinoat.mongodb.net/inkExchangeDB?retryWrites=true&w=majority`;
+    const mongoUrl = `mongodb+srv://${envConfig.MONGO_DB_USERNAME}:${envConfig.MONGO_DB_PASSWORD}@clusterinkexchange.9jinoat.mongodb.net/${envConfig.MONGO_DB}?retryWrites=true&w=majority`;
     await mongoose.connect(mongoUrl, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
+    mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 
     logger.info(`connected to mongo database : ${envConfig.MONGO_DB_NAME}!`);
   } catch (error) {
